49570e6d2ebe2ff40060ed51509f9a11f274150d
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-12-08 18:43:11 +0500

    New solutions add

--- assignment.js
+++ assignment.js
@@ -79,6 +79,67 @@ function humanReadable (seconds) {
   return hours +  ":" + minutes +":" + remain
 }
 */
+/* 
+# Remove duplication
+
+Write a function that accepts an array of number as parameter, and it returns array after removing all duplicated numbers.
+
+Example removeDuplication([1,2,3,2,4,6,2,6,7]) Output [1,3,4,7].
+
+Here 2,6 are duplicated in array, so we should remove all numbers that are duplicated. The output will be 1,3,4,7 after removing 2,6.
+
+Input : [1,2,1,2,1,1,2,2] , Output : [].
+
+Input : [2,5,6,7,5,2,6] , Output: [7].
+
+//solutions
+
+const removeDuplication = arr =>
+  arr.filter(val => arr.indexOf(val) === arr.lastIndexOf(val));
+
+*/
+
+/*
+
+#String incrementer
+
+Your job is to write a function which increments a string, to create a new string.
+
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+
+foo -> foo1
+
+foobar23 -> foobar24
+
+foo0042 -> foo0043
+
+foo9 -> foo10
+
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.
 
 
+// solutions
 
+const incrementString = strng =>
+  strng.replace(/[0-8]?9*$/, val => ++val);
+*/
+
+//  Return Negative
+/*
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Examples
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
+
+// solutions
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+*/
