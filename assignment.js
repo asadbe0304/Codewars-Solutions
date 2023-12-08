fb32bc24a18d4566f31811caed925c9013f84fa9
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-12-08 19:53:20 +0500

    New solutions

--- assignment.js
+++ assignment.js
@@ -281,3 +281,21 @@ class Person {
 
 */
 
+/*
+#8 kyu
+Multiply the number
+Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+multiply(3)==15 // 3 * 5¹
+multiply(10)==250 // 10 * 5²
+multiply(200)==25000 // 200 * 5³
+multiply(0)==0 // 0 * 5¹
+multiply(-3)==-15 // -3 * 5¹
+function multiply(number){
+  const power = Math.abs(number).toString().length;
+const multiply = Math.pow(5, power)
+return number*multiply
+}
+
+console.log(multiply(3));
+ */
