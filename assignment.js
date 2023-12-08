be78e99b2294bf4fd57200c0c5317baccd7ec1c5
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-12-08 18:58:05 +0500

    CodeWarss new solutions 8kyu

--- assignment.js
+++ assignment.js
@@ -143,3 +143,42 @@ function makeNegative(num) {
   return -Math.abs(num);
 }
 */
+/*
+#Duck duck goose
+
+DESCRIPTION:
+The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+Example:
+
+duck_duck_goose([a, b, c, d], 1) should return a.name
+duck_duck_goose([a, b, c, d], 5) should return a.name
+duck_duck_goose([a, b, c, d], 4) should return d.name
+// PHP only
+duck_duck_goose([$a, $b, $c, $d], 1); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 5); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 4); // => $d["name"] 
+*/
+
+// // solutions
+// function duck_duck_goose(players, index) {
+//   const chosenIndex = (index - 1) % players.length;
+//   return players[chosenIndex].name;
+// }
+
+// // Example usage:
+// const a = { name: "Player A" };
+// const b = { name: "Player B" };
+// const c = { name: "Player C" };
+// const d = { name: "Player D" };
+
+// console.log(duck_duck_goose([a, b, c, d], 1)); // Output: Player A
+// console.log(duck_duck_goose([a, b, c, d], 5)); // Output: Player A
+// console.log(duck_duck_goose([a, b, c, d], 4)); // Output: Player D
+//////////////////////////////////////
+/*
+ 
+
+ */
